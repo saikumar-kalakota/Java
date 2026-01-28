5e0cfc2d4eaffd1b1643fd01d6f92e9dd8d4692a
saikumar-kalakota <kalakota.saikumar04@gmail.com>
2026-01-29 00:18:35 +0530

    Sorting Arrays

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        int [] a = { 56, 34, 67, 34, 5675, 345};
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+
+        }
+    }
