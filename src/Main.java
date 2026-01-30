c8be8a4ab75aba2efa2fd7803400132bb84c099a
saikumar-kalakota <kalakota.saikumar04@gmail.com>
2026-01-30 22:12:40 +0530

    Added sorts using Collections post coverting to List

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,10 @@ public class Main {
         int [] arr = { 56, 34, 67, 34, 5675, 345};
 
         //Converting primitive int array to Integer array using Arrays class, stream(arr), boxing into IntStream, reconvert to Array as new Integer
-        Integer [] intArr= Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        Integer [] intArr= Arrays
+                            .stream(arr)
+                            .boxed()
+                            .toArray(Integer[]::new);
         System.out.println("Integer array is :" + Arrays.toString(intArr));
 
         //sub-array of 3 elements (starting from index 0 to index 2) sort
@@ -31,7 +34,7 @@ public class Main {
 
         //reverse sort
 
-        Arrays.sort( intArr, Collections.reverseOrder());
+        Arrays.sort( intArr, Collections.reverseOrder()); //~ AS CRO~~ AS CRO ~ESCROW
         System.out.println("Reverse order");
         System.out.println(Arrays.toString(intArr));
 
@@ -42,6 +45,8 @@ public class Main {
 
         Character [] charArr = convertToCharacterArray(a);
         System.out.println(Arrays.asList(charArr));
+
+        sortArrays();
     }
     
     public static String reverseString(String str){
@@ -60,8 +65,27 @@ public class Main {
         return new String(a)
                 .chars()
                 .mapToObj(c-> (char) c)
-//                .distinct() //optional
-//                .sorted() //optional
                 .toArray(Character[]::new);
     }
+
+    static void sortArrays(){
+        int [] arr =  new int[8];
+        arr = new int [] { 12, 34543, 23, 4545, 345,33, 674, 67};
+        Integer [] intArr = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+
+        System.out.println(Arrays.toString(intArr));
+
+        Arrays.sort(intArr, Collections.reverseOrder());
+        System.out.println(Arrays.toString(intArr));
+
+        List<Integer> lstArr= new ArrayList<>(Arrays.asList(intArr));
+        System.out.println(lstArr);
+
+        Collections.sort(lstArr);
+        System.out.println("Asc order sort using Collections.sort(list) " +lstArr );
+
+        Collections.sort(lstArr, Comparator.comparingInt(Integer::intValue).reversed());
+        System.out.println("Desc order sort using Collections.sort(list, Comparator.comparingInt(Integer::intValue).reversed()) " +lstArr );
+    }
+
     }
