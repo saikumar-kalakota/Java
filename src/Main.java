026f6cd5289f03f69f37b4acd4c9cd62069f4e5d
saikumar-kalakota <kalakota.saikumar04@gmail.com>
2026-01-30 23:29:19 +0530

    String operations. Optional type usage.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -9,13 +11,13 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        int [] arr = { 56, 34, 67, 34, 5675, 345};
+        int[] arr = {56, 34, 67, 34, 5675, 345};
 
         //Converting primitive int array to Integer array using Arrays class, stream(arr), boxing into IntStream, reconvert to Array as new Integer
-        Integer [] intArr= Arrays
-                            .stream(arr)
-                            .boxed()
-                            .toArray(Integer[]::new);
+        Integer[] intArr = Arrays
+                .stream(arr)
+                .boxed()
+                .toArray(Integer[]::new);
         System.out.println("Integer array is :" + Arrays.toString(intArr));
 
         //sub-array of 3 elements (starting from index 0 to index 2) sort
@@ -29,82 +31,116 @@ public class Main {
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
 
-        int searchedInt= 67;
+        int searchedInt = 67;
         int i = Arrays.binarySearch(arr, searchedInt);
-        System.out.println("Searched element " +  searchedInt + "  is found at index   " + i  );
+        System.out.println("Searched element " + searchedInt + "  is found at index   " + i);
 
         //reverse sort
 
-        Arrays.sort( intArr, Collections.reverseOrder()); //~ AS CRO~~ AS CRO ~ESCROW
+        Arrays.sort(intArr, Collections.reverseOrder()); //~ AS CRO~~ AS CRO ~ESCROW
         System.out.println("Reverse order");
         System.out.println(Arrays.toString(intArr));
 
-        String str= reverseString("Monalisa");
-        System.out.println("Reversed string "+ str);
+        String str = reverseString("Monalisa");
+        System.out.println("Reversed string " + str);
 
-        char [] a= {'a', 'e', 'i', 'o','s','a', 'u', 'b'};
+        char[] a = {'a', 'e', 'i', 'o', 's', 'a', 'u', 'b'};
 
-        Character [] charArr = convertToCharacterArray(a);
+        Character[] charArr = convertToCharacterArray(a);
         System.out.println(Arrays.asList(charArr));
 
         sortArrays();
-        int [] arr2 = new int [7];
-        arr2= new int[] {1, 5,3,44,6,33,3};
-        int [] mergedPrimitiveIntsArray= mergeArrays(arr, arr2);
+        int[] arr2 = new int[7];
+        arr2 = new int[]{1, 5, 3, 44, 6, 33, 3};
+        int[] mergedPrimitiveIntsArray = mergeArrays(arr, arr2);
         System.out.println(Arrays.toString(mergedPrimitiveIntsArray));
         int[] array = Arrays.stream(mergedPrimitiveIntsArray).distinct().toArray();
         System.out.println(Arrays.toString(array));
 
+        String cde = "cde";
+        System.out.println("abc" + cde);
+        String c = "abc".substring(2, 3);
+        System.out.println(c);
+        String d = cde.substring(1, 2); //c-> 0, d->1, e->2 ; as beginIndex =1 & endIndex =2 (excluded) only d is extracted.
+        // No of characters extracted = Difference  of indexes
+        // Eg: "smiles".substring(1, 5) returns "mile". 5-1= 4. Only 4 characters are printed starting from Index 1. Hence, m i l e.
+        System.out.println(d);
+        Optional<Map.Entry<Character, Long>> maxAppearedCharacter= mostFrequentCharacter("successes");
+        System.out.println(maxAppearedCharacter);
+
     }
-    
-    public static String reverseString(String str){
-        
-        char [] arr= str.toCharArray();
+
+    public static String reverseString(String str) {
+
+        char[] arr = str.toCharArray();
         StringBuilder reverseStr = new StringBuilder();
-        for(int i=arr.length-1; i>=0; i--) {
-            char c= arr[i];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            char c = arr[i];
             reverseStr.append(c);
         }
         System.out.println(reverseStr);
         return reverseStr.toString();
     }
 
-    static Character [] convertToCharacterArray(char [] a){
+    static Character[] convertToCharacterArray(char[] a) {
         return new String(a)
                 .chars()
-                .mapToObj(c-> (char) c)
+                .mapToObj(c -> (char) c)
                 .toArray(Character[]::new);
     }
 
-    static void sortArrays(){
-        int [] arr =  new int[8];
-        arr = new int [] { 12, 34543, 23, 4545, 345,33, 674, 67};
-        Integer [] intArr = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+    static void sortArrays() {
+        int[] arr = new int[8];
+        arr = new int[]{12, 34543, 23, 4545, 345, 33, 674, 67};
+        Integer[] intArr = Arrays.stream(arr).boxed().toArray(Integer[]::new);
 
         System.out.println(Arrays.toString(intArr));
 
         Arrays.sort(intArr, Collections.reverseOrder());
         System.out.println(Arrays.toString(intArr));
 
-        List<Integer> lstArr= new ArrayList<>(Arrays.asList(intArr));
+        List<Integer> lstArr = new ArrayList<>(Arrays.asList(intArr));
         System.out.println(lstArr);
 
         Collections.sort(lstArr);
-        System.out.println("Asc order sort using Collections.sort(list) " +lstArr );
+        System.out.println("Asc order sort using Collections.sort(list) " + lstArr);
 
         Collections.sort(lstArr, Comparator.comparingInt(Integer::intValue).reversed());
-        System.out.println("Desc order sort using Collections.sort(list, Comparator.comparingInt(Integer::intValue).reversed()) " +lstArr );
+        System.out.println("Desc order sort using Collections.sort(list, Comparator.comparingInt(Integer::intValue).reversed()) " + lstArr);
     }
 
     /**
-     *
      * @param a array of primitive integers
      * @param b array of primitive integers
      * @return array of primitive integers
      */
 
-    static int [] mergeArrays(int [] a, int [] b){
-        int [] mergedArray= IntStream.concat(Arrays.stream(a) , Arrays.stream(b)).toArray();
-        return mergedArray ;
+    static int[] mergeArrays(int[] a, int[] b) {
+        int[] mergedArray = IntStream.concat(Arrays.stream(a), Arrays.stream(b)).toArray();
+        return mergedArray;
     }
+
+    static Optional<Map.Entry<Character, Long>> mostFrequentCharacter(String word){
+        Map<Character,Long> mapOfCharInt= word
+                                            .chars()
+                                            .mapToObj(c-> (char)c)
+                                            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        System.out.println(mapOfCharInt);
+
+        Optional<Map.Entry<Character, Long>> maxFreqCharacter = mapOfCharInt
+                                                                .entrySet()
+                                                                .stream()
+                                                                .max(Map.Entry.comparingByValue());
+        return maxFreqCharacter;
+
     }
+
+    static Optional<Integer> maxValueInList(List<Integer> list){
+
+        return list
+                .stream()
+                        .max(Comparator.comparing(Integer::intValue));
+    }
+
+}
